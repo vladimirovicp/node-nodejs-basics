80cf26bb57f97223b0a30fd5f4e08304af3d5d6b
vladimirovicp <vladimirovicp70@gmail.com>
2023-05-11 19:56:14 +0400

    add: begin create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
-const create = async () => {
-    // Write your code here 
+//create.js - реализовать функцию, которая создает новый файл fresh.txt с содержимым,
+// "I am fresh and young"  внутри папки с files
+// (если файл уже существует, необходимо выдать ошибку с сообщением "FS operation failed")
+
+
+//const fs = require('fs');
+import { open } from 'node:fs';
+
+const create = async (urlFile) => {
+    open(urlFile,'wx',(err,fd) => {
+        if (err){
+            if(err.code === 'EEXIST'){
+                console.log('FS operation failed')
+                return
+            }
+
+        } else {
+            console.log('Допиши запись в файл')
+        }
+    })
+
 };
 
-await create();
+await create('./src/fs/files/fresh.txt');
